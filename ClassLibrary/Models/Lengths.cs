40bcb2aa7b31f15d5c28e93b7ef3a58d00ee8379
Aditey-git <raiaditey@gmail.com>
2026-03-10 16:56:49 +0530

    [Aditey Rai].Refactor - Length Unit with conversion responsibility to enforce Single Responsibility Principle and test coverage

--- ClassLibrary/Models/Lengths.cs
+++ ClassLibrary/Models/Lengths.cs
@@ -10,4 +10,33 @@ using System;
             Yard,
             Centimeters
         }
+
+
+
+         public static class LengthUnitExtensions
+    {
+        public static double ToBaseUnit(this LengthUnit unit, double value)
+        {
+            return unit switch
+            {
+                LengthUnit.Feet => value,
+                LengthUnit.Inch => value / 12.0,
+                LengthUnit.Yard => value * 3.0,
+                LengthUnit.Centimeters => value / 30.48,
+                _ => throw new ArgumentException("Unsupported unit.")
+            };
+        }
+
+        public static double FromBaseUnit(this LengthUnit unit, double baseValue)
+        {
+            return unit switch
+            {
+                LengthUnit.Feet => baseValue,
+                LengthUnit.Inch => baseValue * 12.0,
+                LengthUnit.Yard => baseValue / 3.0,
+                LengthUnit.Centimeters => baseValue * 30.48,
+                _ => throw new ArgumentException("Unsupported unit.")
+            };
+        }
+    }
     }
